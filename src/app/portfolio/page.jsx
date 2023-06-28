8dff78807ee5b7e2345dc78cceff1e23aa9417b4
collinskrotich <kolinsr@gmail.com>
2023-06-28 13:00:38 +0300

    portfolio change in progress

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -3,7 +3,9 @@ import styles from './Portfolio.module.css'
 
 const portfolio = () => {
   return (
-    <div className={styles.container}>portfolio</div>
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+  </div>
   )
 }
 
