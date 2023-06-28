0ee8d7f203f7a40f29fa1e809a5a32dcba609c40
collinskrotich <kolinsr@gmail.com>
2023-06-28 12:42:18 +0300

    Finished the contact page + animations

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import styles from './Contact.module.css'
+import Image from 'next/image'
+import Button from '@/components/Button/Button'
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" 
+          alt="contact" 
+          fill={true}
+          className={styles.img} />
+        </div>
+
+        <form className={styles.form}>
+          <input type="text" placeholder='Name' className={styles.input} />
+          <input type="text" placeholder='Name' className={styles.input} />
+          <textarea 
+          placeholder='Message' 
+          rows={10}
+          cols={30}
+          className={styles.textarea} />
+          <Button url="#" text="Send Message" />
+        </form>
+      </div>
+    </div>
   )
 }
 
