edc8f0f2cbb1a7b772bf672ffd6489fd08420f38
collinskrotich <kolinsr@gmail.com>
2023-06-30 10:42:21 +0300

    Portfolio and blog changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import styles from './Blog.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.container}>
+      <Link href="/blog/1" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            src={"/hero.png"} 
+            alt="hero"
+            width={400}
+            height = {250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+        </Link>
+
+        <Link href="/blog/1" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            src={"/hero.png"} 
+            alt="hero"
+            width={400}
+            height = {250}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+        </Link>
+      </div>
   )
 }
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import styles from './category.module.css'
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
+  console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+          className={styles.img}
+            fill={true}
+            src={"/hero.png"} 
+            alt="hero"
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+          className={styles.img}
+            fill={true}
+            src={"/hero.png"} 
+            alt="hero"
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+          className={styles.img}
+            fill={true}
+            src={"/hero.png"} 
+            alt="hero"
+          />
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
